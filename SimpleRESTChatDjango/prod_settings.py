8a2f12d85940ed0fc989be127b108cf3fb032312
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-28 23:27:51 +0300

    DB add

---
+++ SimpleRESTChatDjango/prod_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webapp',
+        'USER': 'korzhov',
+        'PASSWORD': '745263AA',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
