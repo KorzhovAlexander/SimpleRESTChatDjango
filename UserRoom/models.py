ebd82e611169eba071ed50efdcd6c845e533607b
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-30 22:40:16 +0300

    MessageChatRoom model code rev

--- UserRoom/models.py
+++ UserRoom/models.py
@@ -31,7 +31,21 @@ class ChatRoom(models.Model):
 
 
 class MessageChatRoom(models.Model):
-    room = models.ForeignKey(ChatRoom, verbose_name='Комната чата', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    message = models.TextField(verbose_name='Сообщение', max_length=800)
-    date = models.DateTimeField(verbose_name='Дата отправки сообщения', auto_now_add=True)
+    room = models.ForeignKey(
+        ChatRoom,
+        verbose_name='Комната чата',
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE
+    )
+    message = models.TextField(
+        verbose_name='Сообщение',
+        max_length=800
+    )
+    date = models.DateTimeField(
+        verbose_name='Дата отправки сообщения',
+        auto_now_add=True
+    )
