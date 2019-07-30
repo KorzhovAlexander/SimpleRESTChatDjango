99b866a79dd62991839fd736aaac86f2d427567c
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-30 20:04:34 +0300

    view /room created

--- UserRoom/views.py
+++ UserRoom/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from UserRoom.models import ChatRoom
+from UserRoom.serializers import ChatRoomSerializer
+
 
 # Create your views here.
 
@@ -10,4 +13,6 @@ from rest_framework.response import Response
 class getRoom(APIView):
 
     def get(self, request):
-        return Response({1: 1})
+        room = ChatRoom().objects.all()
+        serializer = ChatRoomSerializer(room, many=True)
+        return Response(serializer.data)
