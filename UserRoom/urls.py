6333a746287269e6cda7154fa534104ec2a24d4b
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-31 22:05:01 +0300

    add getRoomById

--- UserRoom/urls.py
+++ UserRoom/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from UserRoom import views
 
 urlpatterns = [
-    path('', views.getRoom.as_view()),
+    path('', views.getAllRooms.as_view()),
+    path('<int:pk>', views.getRoomById.as_view()),
+
     path('chat/', views.getChat.as_view()),
 ]
