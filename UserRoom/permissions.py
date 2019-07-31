70f79514e86db229e451980a6a8aecc897495f4e
KorzhovAlexander <honeyiamgood@mail.ru>
2019-08-01 00:02:03 +0300

    IsOwner and IsOwnerOrReadOnly create permissions

--- UserRoom/permissions.py
+++ UserRoom/permissions.py
@@ -6,3 +6,14 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.user == request.user
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
